db5741031e73149cdc02fe43f964020da81e7f8d
Reina Rodriguez <rrodriguez.ut@gmail.com>
2021-07-07 13:54:37 -0600

    Fixed MongoDB errors

---
+++ level-5-capstone/models/family.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema 
+
+
+// familyMembers: []
+
+
+const familySchema = new Schema({
+    name: {
+        type: String,
+        unique: true },
+    atHome: Boolean,
+    estReturnTime: Number,
+    location: String
+})
+
+
+
+
+module.exports = mongoose.model("Family", familySchema)
