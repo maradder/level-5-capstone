6294aba325490a63a71ad8d8c1a499ae6e3fa4ad
Marcus Radder <marcusradder@gmail.com>
2021-07-07 15:20:39 -0700

    Merge front and backend

---
+++ level-5-capstone/client/src/context/PersonContext.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, createContext } from "react"
+import axios from "axios"
+const PersonContext = createContext()
+const PersonContextProvider = props => {
+	const [comingBack, setComingBack] = useState([])
+	const [whereGoing, setWhereGoing] = useState()
+	const [whoIsGoing, setWhoIsGoing] = useState([])
+	const [headingOut, setHeadingOut] = useState([])
+	const [updateObj, setUpdateObj] = useState({})
+	const [timeAway, setTimeAway] = useState(5)
+	const [familyMembers, setFamilyMembers] = useState([])
+	const getCurrentFamilyMembers = () => {
+		axios.get("/familymembers").then(res => setFamilyMembers(res.data))
+		return console.log("Initial get")
+	}
+
+	const locations = [
+		{ location: "school" },
+		{ location: "store" },
+		{ location: "gym" },
+		{ location: "work" },
+		{ location: "downtown" },
+		{ location: "cafe" },
+		{ location: "friends" },
+		{ location: "trail" },
+	]
+
+	useEffect(() => {
+		getCurrentFamilyMembers()
+		return () => {
+			console.log("Initial GET request")
+		}
+	}, [])
+
+	useEffect(() => {
+		console.log(familyMembers)
+		return () => {
+			console.log("done")
+		}
+	}, [familyMembers])
+
+	return (
+		<PersonContext.Provider
+			value={{
+				familyMembers,
+				setFamilyMembers,
+				locations,
+				comingBack,
+				setComingBack,
+				setWhereGoing,
+				whereGoing,
+				whoIsGoing,
+				setWhoIsGoing,
+				timeAway,
+				setTimeAway,
+				// checkInFunction,
+			}}
+		>
+			{props.children}
+		</PersonContext.Provider>
+	)
+}
+
+export { PersonContextProvider, PersonContext }
--- level-5-capstone/server.js
+++ level-5-capstone/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
@@ -6,27 +6,22 @@ const morgan = require("morgan")
 
 const family = []
 
-mongoose.connect("mongodb+srv://marcus:TRojans3180@@@clock-db.vmkf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-    {
-
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true,
-    UseFindAndModify: false
-
-    },
-    () => console.log("Connected to the db")
+mongoose.connect(
+	"mongodb+srv://marcus:TRojans3180@@@clock-db.vmkf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		UseFindAndModify: false,
+	},
+	() => console.log("Connected to the db")
 )
-
-
 
 app.use("/", express.json())
 app.use(morgan("dev")) //logs requests to console
 
 app.use("/familymembers", require("./routes/familyRouter.js"))
 
-
-
 app.listen(9000, () => {
-    console.log("hello hello, we're in a place called Vertigo.")
-});
+	console.log("hello hello, we're in a place called Vertigo.")
+})
